26179185520e10999f9d8b94a9e5825e4c6c6912
SQLPaco <galarza.business7@gmail.com>
2025-12-01 23:45:28 -0600

    Added function logic and located variablesto user input

--- project1.c
+++ project1.c
@@ -3,37 +3,47 @@ This is a project for what I learned ~
 
 I Learned: ----------------
 - Variables and Data Types
-- Control Flows if else/ifelse/for loops/while loops/do-while loops
+- Control Flows - if else/ifelse/for loops/while loops/do-while loops
 -----------------------------------------------------------------------------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <string.h>
 
+            int multiply( int monthlysal, int b) {
+                return monthlysal * b;
+                
+            }
+
 int main() {
 
-    char name[25];
-    int age;
-    float monthlysal;
+            char name[25];
+            int age;
+            int monthlysal;
 
-    printf("What is your name?");
-        scanf("%s", name);
-    printf("What is your age?");
-        scanf("%d", age);
-    printf("What is your monthly salary?");
-        scanf("%.2f",monthlysal);
+            printf("What is your name? ");
+                scanf("%s", name);
+            printf("What is your age? ");
+                scanf("%d", &age);
+            printf("What is your gross monthly salary? ");
+                scanf("%d",&monthlysal);
 
-    if(age < 18) {
-        printf("You are a minor.");
-    }
-    else if(age >= 18 && age <= 35 ) {
-        printf("You are a young adult.");
-    }
-    else if(age >= 36 && age <= 60) {
-        printf("Your are an adult");
-    }
-    else if (age >= 61) {
-        printf("You are a senior");
-    }
+            printf("Your name is: %s\n ",name);
 
-    return 0;
-}
+            if(age < 18) {
+                printf("You are a minor.\n ");
+            }
+            else if(age >= 18 && age <= 35 ) {
+                printf("You are a young adult.\n ");
+            }
+            else if(age >= 36 && age <= 60) {
+                printf("Your are an adult.\n ");
+            }
+            else if (age >= 61) {
+                printf("You are a senior.\n ");
+            }
+
+            int result = multiply(monthlysal, 12);
+            printf("%d is your gross yearly salary.\n", result);
+
+            return 0;
+            }
